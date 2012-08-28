91517724c3ccfd0e0067c757a7f52cb082037f29
Daniel Vartanov <dan@vartanov.net>
2012-08-28 12:39:41 +0600

    Fixes RSpec mode. Adds rvm.el

--- daniel.el
+++ daniel.el
@@ -11,9 +11,6 @@
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
-(add-to-list 'load-path (concat dotfiles-dir "/vendor/rspec-mode"))
-(require 'rspec-mode)
-
 (require 'magit)
 
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/gist"))
@@ -28,6 +25,39 @@
 (require 'smooth-scroll)
 (smooth-scroll-mode t)
 
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/slim-mode"))
+(require 'slim-mode)
+
+;; RSpec mode
+
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/rspec-mode"))
+(require 'rspec-mode)
+
+(defadvice rspec-compile (around rspec-compile-around)
+  "Use BASH shell for running the specs because of RVM issues."
+  (let ((shell-file-name "/bin/bash"))
+    ad-do-it))
+(ad-activate 'rspec-compile)
+
+;; RVM
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/rvm.el"))
+(require 'rvm)
+(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
+
+;; Erlang setup
+
+(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.6.5/emacs" load-path))
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+
+(setq erlang-mode-hook
+    (function (lambda ()
+                (setq indent-tabs-mode nil))))
+(setq-default indent-tabs-mode nil)
+(setq indent-tabs-mode nil)
+(setq erlang-indent-level 2)
+(require 'erlang-start)
+
 ;; Emacs frame setting
 
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -37,14 +67,22 @@
 (set-face-attribute 'default nil :height 120)
 
 
+;; Misc options
+
+(setq ido-use-filename-at-point nil)
+(setq next-line-add-newlines nil)
+
 ;; Rebindings
-(global-set-key (kbd "C-x 3") '(lambda () (interactive) (split-window-horizontally) (ido-switch-buffer-other-window)))
-(global-set-key (kbd "C-x 2") '(lambda () (interactive) (split-window-vertically) (ido-switch-buffer-other-window)))
+
+(global-set-key (kbd "C-x 3") '(lambda () (interactive) (split-window-horizontally) (ido-find-file-other-window)))
+(global-set-key (kbd "C-x 2") '(lambda () (interactive) (split-window-vertically) (ido-find-file-other-window)))
 
 (global-set-key (kbd "C-x C-o") 'delete-blank-lines)
 
 (global-set-key (kbd "M-b") 'ruby-backward-sexp)
 (global-set-key (kbd "M-f") 'ruby-forward-sexp)
+
+(global-set-key (kbd "C-S-s") 'replace-string)
 
 ;; (global-set-key (kbd "C-tab") 'other-window)
 ;; (global-set-key (kbd "C-backspace") 'backward-kill-sexp)
