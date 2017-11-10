4a6b8cc018af0e63dd0f73d557a6cb7cb8dfd7a2
Daniel Vartanov <dan@vartanov.net>
2017-11-10 22:37:01 +0000

    Install eimp

---
+++ elpa/eimp-1.4.0/eimp-autoloads.el
@@ -0,0 +1,21 @@
+;;; eimp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "eimp" "eimp.el" (23046 10732 875800 355000))
+;;; Generated autoloads from eimp.el
+
+(autoload 'eimp-mode "eimp" "\
+Toggle Eimp mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eimp-autoloads.el ends here
