f09633b31f930ccaeb8c4b18ad9796ab060d39c0
Daniel Vartanov <dan@vartanov.net>
2018-10-15 09:05:05 +0100

    Highlight line in light mode

--- light.el
+++ light.el
@@ -64,6 +64,9 @@
 ;; Save a list of recent files visited.
 (recentf-mode 1)
 
+;; Highlight current line
+(global-hl-line-mode +1)
+
 ;; ido-mode is like magic pixie dust!
 (when (> emacs-major-version 21)
   (ido-mode t)
