3e07a92f25bf086f7fc7903152653e3628e6ffee
Daniel Vartanov <dan@vartanov.net>
2020-07-04 12:00:06 +0100

    Upgrade markdown-mode, remove unneeded packages

--- elpa/idle-highlight-mode-1.1.3/idle-highlight-mode-autoloads.el
+++ elpa/idle-highlight-mode-1.1.3/idle-highlight-mode-autoloads.el
@@ -1,10 +1,13 @@
 ;;; idle-highlight-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "idle-highlight-mode" "idle-highlight-mode.el"
-;;;;;;  (21934 8983 631631 843000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from idle-highlight-mode.el
 
 (autoload 'idle-highlight-mode "idle-highlight-mode" "\
@@ -12,11 +15,14 @@ Idle-Highlight Minor Mode
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "idle-highlight-mode" '("idle-highlight-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; idle-highlight-mode-autoloads.el ends here
