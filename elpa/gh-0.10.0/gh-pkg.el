d9d0c1f89b1b62cb6d9b5a11ff354fc61d9e7af5
Daniel Vartanov <dan@vartanov.net>
2020-07-04 12:00:31 +0100

    Remove idle-highlight

---
+++ elpa/gh-0.10.0/gh-pkg.el
@@ -0,0 +1,8 @@
+(define-package "gh" "0.10.0" "A GitHub library for Emacs"
+  '((emacs "24.4")
+    (pcache "0.3.1")
+    (logito "0.1"))
+  :commit "d0471dd1ba279072cbf8caf6368f3008d7120f25")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
