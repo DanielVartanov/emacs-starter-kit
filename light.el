3d92fa97ee1783e57c9dcbbabb47ead02b067986
Daniel Vartanov <dan@vartanov.net>
2020-10-15 10:18:42 +0100

    Add global org-capture keybinding

--- light.el
+++ light.el
@@ -32,6 +32,7 @@
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
 
 (setq org-agenda-files (list "~/Dropbox/my-texts/gtd.org"))
 
