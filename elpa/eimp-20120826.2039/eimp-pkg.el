9322566d14324ef9e3cbf3818994da8332dc3075
Daniel Vartanov <dan@vartanov.net>
2020-12-12 13:19:40 +0000

    Reinstall the packages I use

---
+++ elpa/eimp-20120826.2039/eimp-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/daniel/src/.emacs.d-27/elpa/eimp-20120826.2039/eimp.el  -*- no-byte-compile: t -*-
+(define-package "eimp" "20120826.2039" "Emacs Image Manipulation Package" 'nil :commit "2e7536fe6d8f7faf1bad7a8ae37faba0162c3b4f" :keywords '("files" "frames") :authors '(("Matthew P. Hodges" . "MPHodges@member.fsf.org")) :maintainer '("Nic Ferrier" . "nferrier@ferrier.me.uk"))
