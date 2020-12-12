43530a6190adff02409a9c9e5bcbaa562619a984
Daniel Vartanov <dan@vartanov.net>
2020-12-12 12:21:10 +0000

    Completely delete old elpa packages and refresh archives

--- elpa/eproject-0.4/eproject-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; eproject-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "eproject" "eproject.el" (21934 11402 911582
-;;;;;;  476000))
-;;; Generated autoloads from eproject.el
-(require 'eproject)
-
-;;;***
-
-;;;### (autoloads nil nil ("eproject-config.el" "eproject-pkg.el")
-;;;;;;  (21934 11402 931197 748000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; eproject-autoloads.el ends here
--- elpa/rvm-1.4.1/rvm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rvm" "1.4.1" "Emacs integration for rvm" 'nil :commit "b2498a4a6952a32301544e103b31dc3b8df271ee" :keywords '("ruby" "rvm") :authors '(("Yves Senn" . "yves.senn@gmx.ch")) :maintainer '("Yves Senn" . "yves.senn@gmx.ch") :url "http://www.emacswiki.org/emacs/RvmEl")
