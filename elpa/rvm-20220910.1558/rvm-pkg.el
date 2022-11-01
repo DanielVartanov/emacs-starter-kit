8fa9eafa8444123d31a631a0e97d77182a885874
Daniel Vartanov <dan@vartanov.net>
2022-11-01 11:13:57 +0000

    Upgrade all dependencies

---
+++ elpa/rvm-20220910.1558/rvm-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/daniel/.emacs.d/elpa/rvm-20220910.1558/rvm.el  -*- no-byte-compile: t -*-
+(define-package "rvm" "20220910.1558" "Emacs integration for rvm" 'nil :commit "e1e83b5466c132c066142ac63729ba833c530c83" :authors '(("Yves Senn" . "yves.senn@gmx.ch")) :maintainer '("Yves Senn" . "yves.senn@gmx.ch") :keywords '("ruby" "rvm") :url "http://www.emacswiki.org/emacs/RvmEl")
