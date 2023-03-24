51a422f31a90f07fbabbabb724f037f555ab962d
Daniel Vartanov <dan@vartanov.net>
2023-03-24 18:08:53 +0000

    Upgrade dependencies

---
+++ elpa/rg-20230323.1807/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20230323.1807" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.3.0")
+    (wgrep "2.1.10"))
+  :commit "09f619e36cc47dd49d5ba745549e19db8b2e73c4" :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :keywords
+  '("matching" "tools")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
