b64fd1ae5cbd6f12fd951889e452307aed1ff713
Daniel Vartanov <dan@vartanov.net>
2024-11-17 13:11:29 +0000

    Upgrade dependencies

---
+++ elpa/markdown-mode-20241117.307/markdown-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "markdown-mode" "20241117.307"
+  "Major mode for Markdown-formatted text."
+  '((emacs "27.1"))
+  :url "https://github.com/jrblevin/markdown-mode"
+  :commit "1716694217bfb802f768d2353cb801459027c294"
+  :revdesc "1716694217bf"
+  :keywords '("markdown" "github flavored markdown" "itex")
+  :authors '(("Jason R. Blevins" . "jblevins@xbeta.org"))
+  :maintainers '(("Jason R. Blevins" . "jblevins@xbeta.org")))
