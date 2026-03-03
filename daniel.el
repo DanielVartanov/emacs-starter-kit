7ec5fac13936cd7e3b796843e3d5484b49801eb1
Daniel Vartanov <dan@vartanov.net>
2026-03-03 10:52:46 +0000

    Add a note about fonts

--- daniel.el
+++ daniel.el
@@ -1,5 +1,6 @@
 (require 'better-defaults)
 
+;; NOTE: Emacs will automatically fallback to Nerd Fonts collection in case of a missing glyph
 (set-frame-font "Source Code Pro")
 (set-face-attribute 'default nil :height 140)
 
